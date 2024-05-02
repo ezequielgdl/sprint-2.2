a631b95d4d18d076f6f7d432955224cc0bf2a13b
ezequielgdl <108180682+ezequielgdl@users.noreply.github.com>
2024-05-02 12:26:56 +0200

    exercici 4

--- js/shop.js
+++ js/shop.js
@@ -100,16 +100,20 @@ function cleanCart() {
 // Exercise 3
 function calculateTotal() {
   // Calculate total price of the cart using the "cartList" array
-  let result = 0;
   for (const item of cart) {
-    result += item.price * item.quantity;
+    total += item.price * item.quantity;
   }
-  total = result;
 }
 
 // Exercise 4
 function applyPromotionsCart() {
   // Apply promotions to each item in the array "cart"
+  for (const item of cart) {
+    if (item.offer && item.quantity >= item.offer.number) {
+      discount = item.price * (item.offer.percent / 100);
+      item.price -= discount;
+    }
+  }
 }
 
 // Exercise 5
